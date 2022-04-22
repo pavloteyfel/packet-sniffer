7cf487905d0c06f03052c68e18506508f319b6a3
Pavlo Teyfel <pavloteyfel@gmail.com>
2022-04-22 12:52:58 +0200

    feature: Add argparse

--- packetsniffer.py
+++ packetsniffer.py
@@ -1,12 +1,20 @@
+from click import argument
 from scapy.all import sniff, Raw, IP
 from scapy.layers import http
 from colorama import init, Fore
+import argparse
 
 
-init()
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", action="store",
+                        type=str, help="Please specify target interface")
+    return parser.parse_args()
+
 
 def sniff_packet(iface):
     sniff(iface=iface, store=False, prn=process_packet)
+
 
 def get_login_info(packet):
     if packet.haslayer(Raw):
@@ -21,18 +29,18 @@ def process_packet(packet):
     if packet.haslayer(http.HTTPRequest):
 
         ip = packet[IP].src
-        url = packet[http.HTTPRequest].Host.decode() + packet[http.HTTPRequest].Path.decode()
+        url = packet[http.HTTPRequest].Host.decode(
+        ) + packet[http.HTTPRequest].Path.decode()
         method = packet[http.HTTPRequest].Method.decode()
         print("[+]", ip, method, url)
 
         login_info = get_login_info()
         if login_info:
-            print(f"{Fore.RED}[*] Possible credentials: {login_info}{Fore.RESET}")
-
+            print(
+                f"{Fore.RED}[*] Possible credentials: {login_info}{Fore.RESET}")
 
 
 if __name__ == "__main__":
-
-    iface = "wlan0"
-
-    sniff_packet(iface)
+    init()
+    arguments = get_arguments()
+    sniff_packet(arguments.interface)
